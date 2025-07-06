181a56f2bc4dd4e4da2c9c3e37702d8e919ee9fd
Thienphuc <mac@MacBook-Air-cua-MAC.local>
2025-07-07 00:06:08 +0700

    devlop_first_commit

--- test.js
+++ test.js
@@ -1,1 +1,17 @@
 console.log('Thien Phuc')
+
+// promise
+
+let a = 10;
+let b = 2;
+const test = new Promise((resolve, reject) => {
+ let d = a / b;
+ if (d != Infinity) {
+  resolve('Success');
+ }
+ else {
+  reject('Something was wrong!');
+ }
+})
+
+test.then(x => console.log(x)).catch(error => { console.log(error) });
